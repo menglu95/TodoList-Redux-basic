01d4b9a39186f090a29f5b6b9450ac079c4d9913
LuMeng9 <LuMeng9@outlook.com>
2020-06-11 23:51:29 +0800

    update

--- src/containers/VisibleTodoList.js
+++ src/containers/VisibleTodoList.js
@@ -24,13 +24,23 @@ const getVisibleTodos = (todos, filter) => {
 };
 
 /**
- *
+ * To use connect(), you need to define a special function called mapStateToProps
+ * that describes how to transform the current Redux store state into the props you want to pass to a presentational component you are wrapping.
+ * For example, VisibleTodoList needs to calculate todos to pass to the TodoList, so we define a function that filters the state.todos according to the state.visibilityFilter,
+ * and use it in its mapStateToProps:
  */
 
 const mapStateToProps = state => ({
   todos: getVisibleTodos(state.todos, state.visibilityFilter)
 });
 
+/**
+ * In addition to reading the state, container components can dispatch actions.
+ * In a similar fashion, you can define a function called mapDispatchToProps() that
+ * receives the dispatch() method and returns callback props that you want to inject into the presentational component.
+ * For example, we want the VisibleTodoList to inject a prop called onTodoClick into the TodoList component,
+ * and we want onTodoClick to dispatch a TOGGLE_TODO action:
+ */
 const mapDispatchToProps = dispatch => ({
   toggleTodo: id => dispatch(toggleTodo(id))
 });
