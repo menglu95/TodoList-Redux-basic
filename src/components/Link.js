8e96e91c4651470c4158235413e9b4a47671b4c6
LuMeng9 <LuMeng9@outlook.com>
2020-06-15 22:39:18 +0800

    update

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 const Link = ({ active, children, onClick }) => (
   <button
@@ -12,11 +11,5 @@ const Link = ({ active, children, onClick }) => (
     {children}
   </button>
 );
-
-Link.propTypes = {
-  active: PropTypes.bool.isRequired,
-  children: PropTypes.node.isRequired,
-  onClick: PropTypes.func.isRequired
-};
 
 export default Link;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
 import Todo from "./Todo";
 
 const TodoList = ({ todos, toggleTodo }) => (
@@ -9,21 +8,5 @@ const TodoList = ({ todos, toggleTodo }) => (
     ))}
   </ul>
 );
-
-/**
- * If you are unfamiliar about the propTypes,
- * you learn about it from https://blog.bitsrc.io/understanding-react-proptypes-type-checking-in-react-9648a62ce12e
- */
-
-TodoList.propTypes = {
-  todos: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      completed: PropTypes.bool.isRequired,
-      text: PropTypes.string.isRequired
-    }).isRequired
-  ).isRequired,
-  toggleTodo: PropTypes.func.isRequired
-};
 
 export default TodoList;
