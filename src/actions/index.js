42d48259bac00e3f52e08668f94d35aea77e0b29
LuMeng9 <LuMeng9@outlook.com>
2020-06-15 20:35:10 +0800

    update

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+// import { filter } from "minimatch";
+
 /**
  *  action creators
  *
@@ -5,24 +7,57 @@
 
 let nextTodoId = 0;
 
-export const addTodo = text => ({
-  type: "ADD_TODO",
-  id: nextTodoId++,
-  text
-});
+// export const addTodo = text => ({
+//   type: "ADD_TODO",
+//   id: nextTodoId++,
+//   text
+// });
 
-export const setVisibilityFilter = filter => ({
-  type: "SET_VISIBILITY_FILTER",
-  filter
-});
+// export const setVisibilityFilter = filter => ({
+//   type: "SET_VISIBILITY_FILTER",
+//   filter
+// });
 
-export const toggleTodo = id => ({
-  type: "TOGGLE_TODO",
-  id
-});
+// export const toggleTodo = id => ({
+//   type: "TOGGLE_TODO",
+//   id
+// });
 
-export const VisibilityFilters = {
+// export const VisibilityFilters = {
+//   SHOW_ALL: "SHOW_ALL",
+//   SHOW_COMPLETED: "SHOW_COMPLETED",
+//   SHOW_ACTIVE: "SHOW_ACTIVE"
+// };
+
+const addTodo = text => {
+  const obj = {
+    type: "ADD_TODO",
+    id: nextTodoId++,
+    text
+  };
+  return obj;
+};
+
+const setVisibilityFilter = filter => {
+  const obj = {
+    type: "SET_VISIBILITY_FILTER",
+    filter
+  };
+  return obj;
+};
+
+const toggleTodo = id => {
+  const obj = {
+    type: "TOGGLE_TODO",
+    id
+  };
+  return obj;
+};
+
+const VisibilityFilters = {
   SHOW_ALL: "SHOW_ALL",
   SHOW_COMPLETED: "SHOW_COMPLETED",
   SHOW_ACTIVE: "SHOW_ACTIVE"
 };
+
+export { addTodo, setVisibilityFilter, toggleTodo, VisibilityFilters };
